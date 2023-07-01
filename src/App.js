627ecfb1b826f729689c6cac54402af048757095
Carl <zhenyuc@outlook.com>
2023-07-01 14:17:15 +1000

    saved for nothing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css"
 import FullScreenHeader from "./components/themes/Header"
 import Homepage from "./pages/HomePage"
+import { Routes, Route, useLocation } from "react-router-dom"
 
 function App() {
     return (
-        <div style={{ position: "relative" }}>
-            <div id="main-wrapper">
-                <FullScreenHeader></FullScreenHeader>
-                <div id="content" role="main">
-                    <Homepage />
+        <>
+            <div style={{ position: "relative" }}>
+                <div id="main-wrapper">
+                    <FullScreenHeader></FullScreenHeader>
+                    <div id="content" role="main">
+                        <Homepage></Homepage>
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<Homepage></Homepage>} />
+                    </Routes>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 // for rtl format comment out bootstrap.min.css and uncomment bootstrap.rtl.min.css
 import "bootstrap/dist/css/bootstrap.min.css"
 // import "bootstrap/dist/css/bootstrap.rtl.min.css";
@@ -15,4 +16,8 @@ import "./index.scss"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
