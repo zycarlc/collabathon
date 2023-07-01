95908c6eadea318621f16a071a46e54831350045
Carl <zhenyuc@outlook.com>
2023-07-01 12:59:13 +1000

    navbar init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
+// for rtl format comment out bootstrap.min.css and uncomment bootstrap.rtl.min.css
+import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.rtl.min.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+import "./sass/stylesheet.scss"
+
+import "./index.scss"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
